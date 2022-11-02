5d8ef8329df27e8a45e7fca1083c711ace1619e8
Руслан Мамедов <tov-stalyn@yandex.ru>
2022-11-02 13:47:32 +0400

    final annotation fix

--- homework.py
+++ homework.py
@@ -141,7 +141,7 @@ def main(training: Training) -> None:
 
 
 if __name__ == '__main__':
-    packages: tuple[str, list[int]] = [
+    packages: list[tuple[str, list[int]]] = [
         ('SWM', [720, 1, 80, 25, 40]),
         ('RUN', [15000, 1, 75]),
         ('WLK', [9000, 1, 75, 180]),
